04ece3c9651324b41afb8b72422d1d16feae8991
Sehllychka <141375328+Sehllychka@users.noreply.github.com>
2024-03-05 16:58:52 +0300

    Update User.java

--- 3.1.5 Rest/src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ 3.1.5 Rest/src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.FetchMode;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
